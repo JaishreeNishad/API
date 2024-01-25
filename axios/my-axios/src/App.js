286fadcb80cf55eb68fe121636d903ab80751772
JaishreeNishad <Jaysh2222@gmail.com>
2024-01-25 15:35:16 +0530

    Data fetch using axios

--- axios/my-axios/src/App.js
+++ axios/my-axios/src/App.js
@@ -1,20 +1,30 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
 
 function App() {
 
-  
+  const [myData, setMyData] = useState([]) // we are using useState for storing data//
 
   useEffect(()=>{
     axios.get("https://jsonplaceholder.typicode.com/posts") // read data with the help of get method//
-    .then((res)=>{
-         console.log(res)
-    })
+    .then((res)=>
+         setMyData(res.data))
   },[])
   return (
     <div>
       <h1>Axios Tutorial...</h1>
+      {myData.map((post)=>{
+        const {id, title, body} = post; //dstructuring//
+
+        return(
+          <div  key={id}>
+            <h1>{title}</h1>
+            <p>{body}</p>
+          </div>
+          
+        )
+      })}
       
     </div>
   );
